4aead73b39ce14c2675049b4567fda119542546c
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-02-13 23:04:39 +0100

    mongoDB

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-// console.log("HELLO VLAD")
-const fs = require("node:fs/promises");
- 
-console.log(fs)
+ require("dotenv").config();
+ const express = require("express")
+
+ const PORT = process.env.PORT || 5000;
+
+ const app = express();
+
+ app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
