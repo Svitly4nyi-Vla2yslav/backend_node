0d8ebb85ee371b7d8a2257f41b870f16f68be65f
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-03-01 22:11:07 +0100

    clg

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const { Command } = require('commander');
-const program = new Command();
+// const { Command } = require('commander');
+// const program = new Command();
 
-program
-    .name('pet-app')
-    .description('Amazing pet app')
-    .version('1.0.0');
+// program
+//     .name('pet-app')
+//     .description('Amazing pet app')
+//     .version('1.0.0');
 
-program
-    .command('get-animal')
-    .description('Get one or many animals')
-    .argument('<string>', 'string to split')
-    .option('-i, --id <id>', 'animal`s id')
-    .action((options) => {
+// program
+//     .command('get-animal')
+//     .description('Get one or many animals')
+//     .argument('<string>', 'string to split')
+//     .option('-i, --id <id>', 'animal`s id')
+//     .action((options) => {
        
-        console.log(options);
-    });
+//         console.log(options);
+//     });
 
-program.parse();
+// program.parse();
+// console.log("hello vlad")
+
