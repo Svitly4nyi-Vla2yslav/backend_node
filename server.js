4e93e95b694761ce85b9af7adf995ba66ba09e15
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-03-10 20:59:48 +0100

    DB_HOST

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose")
 const app = require('./app')
 
-const DB_HOST = "mongodb+srv://Vladyslav:nClNIxmkyO1HvM5e@cluster0.oytqrp5.mongodb.net/users?retryWrites=true&w=majority&appName=Cluster0";
+// const DB_HOST = "mongodb+srv://Vladyslav:nClNIxmkyO1HvM5e@cluster0.oytqrp5.mongodb.net/users?retryWrites=true&w=majority&appName=Cluster0";
 
-// const {DB_HOST, PORT = 3000} = process.env;
+const {DB_HOST, PORT = 3000} = process.env;
 
-const PORT = 3000; 
+// const PORT = 3000; 
 
 mongoose.set('strictQuery', true)
 
